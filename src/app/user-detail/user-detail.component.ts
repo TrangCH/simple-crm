05caef2ea5be7531b4fdcfa6e13c0c01ed3b08c2
TrangCH <camtrang@web.de>
2022-02-07 23:48:48 +0100

    user-detail.component: Copy an object correctly.

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -44,11 +44,11 @@ export class UserDetailComponent implements OnInit {
 
   editMenu() {
     const dialog = this.dialog.open(DialogEditAddressComponent);
-    dialog.componentInstance.user = this.user;
+    dialog.componentInstance.user = new User(this.user.toJSON());
   }
 
   editUserDetail() {
     const dialog = this.dialog.open(DialogEditUserComponent);
-    dialog.componentInstance.user = this.user;
+    dialog.componentInstance.user = new User(this.user.toJSON());
   }
 }
